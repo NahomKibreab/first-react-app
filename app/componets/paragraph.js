8854e39b6970ec89ad6a46f601875da0adbdb5aa
NahomKibreab <nknahom@gmail.com>
2023-09-30 12:08:18 -0600

    Add next.js

---
+++ app/componets/paragraph.js
@@ -0,0 +1,9 @@
+export default function Paragraph() {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis laudantium
+      ipsa non iure, itaque quaerat. Ea explicabo, excepturi inventore enim cum
+      omnis molestias! Porro adipisci eum vero quo, natus deleniti.
+    </p>
+  );
+}
